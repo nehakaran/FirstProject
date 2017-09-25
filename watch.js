551f582d04dd51e37c569870d64c64b6404962b8
Neha Rathore <rathodneha90@gmail.com>
2017-09-25 22:39:08 +0530

    changes regarding display

--- watch.js
+++ watch.js
@@ -1,5 +1,6 @@
 function setup() {
  createCanvas(400, 400);
+ angleMode(DEGREES);
 }
 
 function draw() {
@@ -9,7 +10,17 @@ function draw() {
  let min = minute();
  let sec = second();
  
- fill(255);
- noStroke();
- text(hr + ':' + min + ':' + sec, 10, 200);
+ strokeWeight(8);
+ stroke(255);
+ noFill();
+ ellipse(200, 200, 300, 300);
+ 
+ strokeWeight(4);
+ stroke(255, 100, 150);
+ let end = map(sec, 0, 60, 0, 360);
+ arc(200, 200, 300, 300, 0, 360, end);
+ 
+// fill(255);
+ //noStroke();
+ //text(hr + ':' + min + ':' + sec, 10, 200);
 }
